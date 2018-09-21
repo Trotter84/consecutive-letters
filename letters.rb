97eaeb5755b0c4bb2a9a7abbf182323bfc15bebe
Daniel Trotter <flare84@gmail.com>
2018-09-21 09:55:50 -0600

    working

--- letters.rb
+++ letters.rb
@@ -1,13 +1,13 @@
 def intro
-  puts "Give me a word."
+  puts "Give me a phrase, I'll give you the word with the highest consecutive letters."
   user_input
 end
 
 def user_input
   print "> "
   word = gets.strip.chomp
-
-  puts "The highest repeated letters is in: #{word}"
+  answer = word.split.max_by{|w| w.length - w.chars.uniq.length}
+  puts "The highest repeated letters is in: #{answer}"
   exit_program
 end
 
